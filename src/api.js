766b08b382737a78f869b003836fc5f6c03ea96f
Himanshi Agrawal <99007273+ifkrishi@users.noreply.github.com>
2023-07-07 01:45:37 +0530

    deploying my weather api project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "df08e4a3f0mshd0c335014ff7ffcp142d29jsn43587a1d8c0c",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5f0950fb2191ddeeee2ca399240ecea9"; // enter your key from openweather API
